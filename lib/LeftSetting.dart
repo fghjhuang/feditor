b1c9eeccea68fa2e151cdb1cec22ffeb67d47e6b
davidhuang <david.huang@jswpac.com.cn>
2022-05-28 12:00:57 +0800

    update

--- lib/LeftSetting.dart
+++ lib/LeftSetting.dart
@@ -89,9 +89,11 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
                             _selected(index);
                           },
                           child: Container(
-                            color: _accountTree[index].cur==_selectedNode?.cur?Colors.black54:Colors.transparent,
+                            color: (_accountTree[index].cur==_selectedNode?.cur)&&
+                                (_accountTree[index].parent==_selectedNode?.parent)?Colors.black54:Colors.transparent,
                             child: Row(
                               children: [
+                                SizedBox(width: 20*_findPatentPos(_accountTree[index]),),
                                 Icon(_getItemType(index)=="tree"?Icons.folder:Icons.insert_drive_file),
                                 Text(_getItem(index))
                               ],
@@ -110,6 +112,13 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
   Map<int,dynamic> _map={};
   Map<String,dynamic> _smap={};
   //获取内容
+  double _findPatentPos(Node node){
+    if(node.parent!=0){
+      return node.level.toDouble();
+    }else{
+      return 0;
+    }
+  }
   String _getItem(int index){
     return _accountTree[index].name;
   }
@@ -129,6 +138,20 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
     setState(() {
 
     });
+  }
+
+  //整理整树
+  _sortTree(){
+    List<Node> _tmp = [];
+    _tmp.addAll(_accountTree);
+    _accountTree.clear();
+    for(int i=0;i<_tmp.length;i++){
+      if(_tmp[i].level==0){
+        _accountTree.add(_tmp[i]);
+      }
+
+    }
+
   }
 
   void _addFileUI() {
@@ -247,15 +270,22 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
     Node _file = Node();
 
     _file.name = name;
-    if(_selectedNode?.type=="node"){
+
+    if(_selectedNode?.type=="node"){//如果当前选择的是node，就直接在最后添加一个文件就可以
       int parent = _selectedNode?.parent??0;
+      _file.level =_selectedNode?.level??0;
+          _file.parent=parent;
+      List<Node> list = _accountTree.where((element) => element.parent==parent).toList();
+      _file.cur = list.length+1;
+    }else if(_selectedNode?.type=="tree"){ //如果当前选择的是tree，就要在tree里面添加node
+      int parent = _selectedNode?.cur??0;
+      _file.level =(_selectedNode?.level??0)+1;
       _file.parent=parent;
       List<Node> list = _accountTree.where((element) => element.parent==parent).toList();
       _file.cur = list.length+1;
-    }else if(_selectedNode?.type=="tree"){
-
-    }else{
+    }else{ //没有选择的情况下就是在根节点添加
       int parent = _selectedNode?.parent??0;
+      _file.level =_selectedNode?.level??0;
       _file.parent=parent;
       List<Node> list = _accountTree.where((element) => element.parent==parent).toList();
       _file.cur = list.length+1;
@@ -263,20 +293,27 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
     _file.show=true;
     _file.type="node";
     _accountTree.add(_file);
+    _sortTree();
   }
 
   void _addTree(String name){
     Node _file = Node();
     _file.name = name;
-    if(_selectedNode?.type=="node"){
+    if(_selectedNode?.type=="node"){//如果当前选择的是node，那么添加在同一tree下面
       int parent = _selectedNode?.parent??0;
+      _file.level =_selectedNode?.level??0;
       _file.parent=parent;
       List<Node> list = _accountTree.where((element) => element.parent==parent).toList();
       _file.cur = list.length+1;
-    }else if(_selectedNode?.type=="tree"){
-
-    }else{
+    }else if(_selectedNode?.type=="tree"){ //如果当前选择的是tree，那么添加在选择的tree下面
+      int parent = _selectedNode?.cur??0;
+      _file.level =(_selectedNode?.level??0)+1;
+      _file.parent=parent;
+      List<Node> list = _accountTree.where((element) => element.parent==parent).toList();
+      _file.cur = list.length+1;
+    }else{ //如果没有选择，就添加到当前节点
       int parent = _selectedNode?.parent??0;
+      _file.level =_selectedNode?.level??0;
       _file.parent=parent;
       List<Node> list = _accountTree.where((element) => element.parent==parent).toList();
       _file.cur = list.length+1;
@@ -284,19 +321,21 @@ class _FileBrowserPageState extends State<FileBrowserPage> {
     _file.show=true;
     _file.type="tree";
     _accountTree.add(_file);
+    _sortTree();
   }
 }
 
 class Node{
-  int parent=0;
-  int cur=0;
-  String name="";
-  bool show=true;
+  int level=0; //层级
+  int parent=0; //父节点
+  int cur=0;  //当前位置
+  String name="";  //名字
+  bool show=true;  //是否展示
   String type="node"; //node tree
 
   @override
   String toString(){
-    return "(parent:$parent,cur:$cur,name:$name,show:$show,type:$type)";
+    return "(level:$level,parent:$parent,cur:$cur,name:$name,show:$show,type:$type)";
   }
 }
 
